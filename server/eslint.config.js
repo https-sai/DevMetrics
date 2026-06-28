65a43f7aa786fa5a2cb66aae9ed6452631e1ce11
sahmed9123 <38222638+https-sai@users.noreply.github.com>
2026-06-28 00:38:50 -0400

    wee

---
+++ server/eslint.config.js
@@ -0,0 +1,18 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs",
+      globals: globals.node,
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+];
